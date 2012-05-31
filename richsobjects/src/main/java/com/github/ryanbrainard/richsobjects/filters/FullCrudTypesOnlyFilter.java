172e2903f7312ae6ba77f21428b1c28262ab0a1f
Ryan Brainard <brainard@heroku.com>
2012-05-30 23:17:10 -0700

    Make FullCrudTypesOnlyFilter operate on BasicSObjectDescription

--- richsobjects/src/main/java/com/github/ryanbrainard/richsobjects/filters/FullCrudTypesOnlyFilter.java
+++ richsobjects/src/main/java/com/github/ryanbrainard/richsobjects/filters/FullCrudTypesOnlyFilter.java
@@ -1,20 +1,20 @@
 package com.github.ryanbrainard.richsobjects.filters;
 
-import com.github.ryanbrainard.richsobjects.api.model.SObjectDescription;
+import com.github.ryanbrainard.richsobjects.api.model.BasicSObjectDescription;
 
 import java.util.Iterator;
 
 /**
  * @author Ryan Brainard
  */
-public class FullCrudTypesOnlyFilter extends IteratorFilter<SObjectDescription> {
+public class FullCrudTypesOnlyFilter extends IteratorFilter<BasicSObjectDescription> {
 
-    public FullCrudTypesOnlyFilter(Iterator<SObjectDescription> filteree) {
+    public FullCrudTypesOnlyFilter(Iterator<BasicSObjectDescription> filteree) {
         super(filteree);
     }
 
     @Override
-    boolean canBeNext(SObjectDescription maybeNext) {
+    boolean canBeNext(BasicSObjectDescription maybeNext) {
         return maybeNext.isCreateable() &&
                maybeNext.isQueryable()  &&
                maybeNext.isUpdateable() &&

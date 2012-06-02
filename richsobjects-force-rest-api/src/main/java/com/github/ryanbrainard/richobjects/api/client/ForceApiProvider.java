77960595db1cf6835a42d9908139a2b1991eb3a6
Ryan Brainard <brainard@heroku.com>
2012-06-02 13:51:46 -0700

    Update ForceApiProvider with full service support for queries and recent items

--- richsobjects-force-rest-api/src/main/java/com/github/ryanbrainard/richobjects/api/client/ForceApiProvider.java
+++ richsobjects-force-rest-api/src/main/java/com/github/ryanbrainard/richobjects/api/client/ForceApiProvider.java
@@ -1,12 +1,10 @@
 package com.github.ryanbrainard.richobjects.api.client;
 
-import com.force.api.ApiSession;
-import com.force.api.DescribeGlobal;
-import com.force.api.DescribeSObject;
-import com.force.api.ForceApi;
+import com.force.api.*;
 import com.github.ryanbrainard.richsobjects.api.client.SfdcApiClient;
 import com.github.ryanbrainard.richsobjects.api.client.SfdcApiClientProvider;
 import com.github.ryanbrainard.richsobjects.api.model.*;
+import com.github.ryanbrainard.richsobjects.api.model.QueryResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +13,7 @@ import java.util.Map;
 /**
  * @author Ryan Brainard
  */
+@SuppressWarnings("UnusedDeclaration")
 public class ForceApiProvider implements SfdcApiClientProvider {
     
     @Override
@@ -29,24 +28,24 @@ public class ForceApiProvider implements SfdcApiClientProvider {
             public GlobalDescription describeGlobal() {
                 return new GlobalDescription() {
 
-                    private final DescribeGlobal describeGlobal = api.describeGlobal();
+                    private final DescribeGlobal srcDescribeGlobal = api.describeGlobal();
 
                     @Override
                     public String getEncoding() {
-                        throw new UnsupportedOperationException();
+                        return srcDescribeGlobal.getEncoding();
                     }
 
                     @Override
                     public Integer getMaxBatchSize() {
-                        throw new UnsupportedOperationException();
+                        return srcDescribeGlobal.getMaxBatchSize();
                     }
 
                     @Override
                     public List<BasicSObjectDescription> getSObjects() {
-                        final List<DescribeSObject> srcDescs = api.describeGlobal().getSObjects();
+                        final List<DescribeSObjectBasic> srcDescs = api.describeGlobal().getSObjects();
                         final List<BasicSObjectDescription> targetDescs = new ArrayList<BasicSObjectDescription>(srcDescs.size());
-                        for (final DescribeSObject d : srcDescs) {
-                            targetDescs.add(toSObjectDescription(d));
+                        for (final DescribeSObjectBasic d : srcDescs) {
+                            targetDescs.add(toBasicSObjectDescription(d));
                         }
                         return targetDescs;
                     }
@@ -55,7 +54,19 @@ public class ForceApiProvider implements SfdcApiClientProvider {
 
             @Override
             public BasicSObjectInformation describeSObjectBasic(String type) {
-                throw new UnsupportedOperationException();
+                final DiscoverSObject<Map> disc = api.discoverSObject(type, Map.class);
+
+                return new BasicSObjectInformation() {
+                    @Override
+                    public BasicSObjectDescription getBasicObjectDescribe() {
+                        return toBasicSObjectDescription(disc.getObjectDescribe());
+                    }
+
+                    @Override
+                    public List<Map<String, ?>> getRecentItems() {
+                        return listDeepCast(disc.getRecentItems());
+                    }
+                };
             }
 
             @Override
@@ -86,126 +97,173 @@ public class ForceApiProvider implements SfdcApiClientProvider {
 
             @Override
             public QueryResult query(String soql) {
-                throw new UnsupportedOperationException();
+                return toQueryResult(api.query(soql));
             }
 
             @Override
             public QueryResult queryMore(String nextRecordsUrl) {
-                throw new UnsupportedOperationException();
+                return toQueryResult(api.queryMore(nextRecordsUrl));
             }
         };
     }
+
+    protected static QueryResult toQueryResult(final com.force.api.QueryResult<Map> srcQueryResult) {
+        return new QueryResult() {
+            @Override
+            public int getTotalSize() {
+                return srcQueryResult.getTotalSize();
+            }
+
+            @Override
+            public boolean isDone() {
+                return srcQueryResult.isDone();
+            }
+
+            @Override
+            public List<Map<String, ?>> getRecords() {
+                return listDeepCast(srcQueryResult.getRecords());
+            }
+
+            @Override
+            public String getNextRecordsUrl() {
+                return srcQueryResult.getNextRecordsUrl();
+            }
+        };
+    }
+
+    private static <S,T> List<T> listDeepCast(List<S> srcRecords) {
+        final List<T> tgtRecords = new ArrayList<T>(srcRecords.size());
+        for (S record : srcRecords) {
+            //noinspection unchecked
+            tgtRecords.add((T) record);
+        }
+        return tgtRecords;
+    }
+
+    protected static BasicSObjectDescription toBasicSObjectDescription(final DescribeSObjectBasic src) {
+        return _toSObjectDescription(src, null, null);
+    }
+
+    protected static SObjectDescription toSObjectDescription(final DescribeSObject src) {
+        return _toSObjectDescription(src, src.getFields(), src.getChildRelationships());
+    }
     
-    private static SObjectDescription toSObjectDescription(final DescribeSObject src) {
+    private static SObjectDescription _toSObjectDescription(final DescribeSObjectBasic basic, 
+                                                            final List<DescribeSObject.Field> fields, 
+                                                            final List<DescribeSObject.ChildEntity> childRelationships) {
         return new SObjectDescription() {
             public String getName() {
-                return src.getName();
+                return basic.getName();
             }
 
             @Override
             public String getLabel() {
-                return src.getLabel();
+                return basic.getLabel();
             }
 
             @Override
             public Boolean isCustom() {
-                return src.isCustom();
+                return basic.isCustom();
             }
 
             @Override
             public String getKeyPrefix() {
-                return src.getKeyPrefix();
+                return basic.getKeyPrefix();
             }
 
             @Override
             public String getLabelPlural() {
-                return src.getLabelPlural();
+                return basic.getLabelPlural();
             }
 
             @Override
             public Boolean isLayoutable() {
-                return src.isLayoutable();
+                return basic.isLayoutable();
             }
 
             @Override
             public Boolean isActivateable() {
-                return src.isActivateable();
+                return basic.isActivateable();
             }
 
             @Override
             public Boolean isUpdateable() {
-                return src.isUpdateable();
+                return basic.isUpdateable();
             }
 
             @Override
             public Map<String, String> getUrls() {
-                return src.getUrls();
+                return basic.getUrls();
             }
 
             @Override
             public Boolean isCreateable() {
-                return src.isCreateable();
+                return basic.isCreateable();
             }
 
             @Override
             public Boolean isDeletable() {
-                return src.isDeletable();
+                return basic.isDeletable();
             }
 
             @Override
             public Boolean isFeedEnabled() {
-                return src.isFeedEnabled();
+                return basic.isFeedEnabled();
             }
 
             @Override
             public Boolean isQueryable() {
-                return src.isQueryable();
+                return basic.isQueryable();
             }
 
             @Override
             public Boolean isReplicateable() {
-                return src.isReplicateable();
+                return basic.isReplicateable();
             }
 
             @Override
             public Boolean isRetrieveable() {
-                return src.isRetrieveable();
+                return basic.isRetrieveable();
             }
 
             @Override
             public Boolean isUndeletable() {
-                return src.isUndeletable();
+                return basic.isUndeletable();
             }
 
             @Override
             public Boolean isTriggerable() {
-                return src.isTriggerable();
+                return basic.isTriggerable();
             }
 
             @Override
             public Boolean isMergeable() {
-                return src.isMergeable();
+                return basic.isMergeable();
             }
 
             @Override
             public Boolean isDeprecatedAndHidden() {
-                return src.isDeprecatedAndHidden();
+                return basic.isDeprecatedAndHidden();
             }
 
             @Override
             public Boolean isCustomSetting() {
-                return src.isCustomSetting();
+                return basic.isCustomSetting();
             }
 
             @Override
             public Boolean isSearchable() {
-                return src.isSearchable();
+                return basic.isSearchable();
             }
 
             @Override
             public List<Field> getFields() {
-                final List<Field> fs = new ArrayList<Field>(src.getFields().size());
-                for (final DescribeSObject.Field f : src.getFields()) {
+                if (fields == null) {
+                    return null;
+                }
+                
+                final List<Field> fs = new ArrayList<Field>(fields.size());
+                for (final DescribeSObject.Field f : fields) {
                     fs.add(new Field() {
                         public Integer getLength() {
                             return f.getLength();
@@ -330,8 +388,12 @@ public class ForceApiProvider implements SfdcApiClientProvider {
 
             @Override
             public List<ChildEntity> getChildRelationships() {
-                List<ChildEntity> ces  = new ArrayList<ChildEntity>(src.getChildRelationships().size());
-                for (final DescribeSObject.ChildEntity ce : src.getChildRelationships()) {
+                if (childRelationships == null) {
+                    return null;
+                }
+                
+                List<ChildEntity> ces = new ArrayList<ChildEntity>(childRelationships.size());
+                for (final DescribeSObject.ChildEntity ce : childRelationships) {
                     ces.add(new ChildEntity() {
                         public String getField() {
                             return ce.getField();

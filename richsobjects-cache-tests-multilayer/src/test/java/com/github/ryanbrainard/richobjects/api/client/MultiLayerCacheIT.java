2acb08e32a6726d07c3f825f01d3513d500905d9
Ryan Brainard <brainard@heroku.com>
2012-06-14 23:04:22 -0700

    multilayer caching test

--- richsobjects-cache-tests-multilayer/src/test/java/com/github/ryanbrainard/richobjects/api/client/MultiLayerCacheIT.java
+++ richsobjects-cache-tests-multilayer/src/test/java/com/github/ryanbrainard/richobjects/api/client/MultiLayerCacheIT.java
@@ -1,15 +1,17 @@
 package com.github.ryanbrainard.richobjects.api.client;
 
+import com.github.ryanbrainard.richsobjects.RichSObject;
 import com.github.ryanbrainard.richsobjects.api.client.*;
+import com.sun.jersey.api.client.UniformInterfaceException;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.*;
 
 /**
  * @author Ryan Brainard
@@ -42,6 +44,31 @@ public class MultiLayerCacheIT extends AbstractRichSObjectServiceIT {
 
         final SfdcApiClient core = getFieldValue(middle, "apiClient");
         assertEquals(core.getClass().getSimpleName(), "SfdcApiJerseyClient");
+    }
+
+    @Test
+    public void testInvalidation() throws Exception {
+        final SfdcApiClient rawClient = new SfdcApiJerseyClientProvider().get(new SfdcApiTestSessionProvider().getAccessToken(), new SfdcApiTestSessionProvider().getApiEndpoint(), "v24.0");
+
+        final String account = "Account";
+        final RichSObject test1 = service.insert(service.of(account).getField("Name").setValue("TEST1"));
+        final String id = test1.getField("Id").asString();
+        assertEquals(test1.getField("Name").asString(), "TEST1");
+
+        final RichSObject test2 = service.update(test1.getField("Name").setValue("TEST2"));
+        assertEquals(test2.getField("Name").asString(), "TEST2");
+
+        rawClient.updateSObject(account, id, Collections.singletonMap("Name", "TEST3"));
+        assertEquals(rawClient.getSObject(account, id).get("Name"), "TEST3");
+        assertEquals(service.fetch(account, id).getField("Name").asString(), "TEST2");
+
+        service.delete(test2);
+        try {
+            service.fetch(account, id);
+            fail();
+        } catch (UniformInterfaceException e) {
+            assertTrue(e.getResponse().getEntity(String.class).contains("NOT_FOUND"));
+        }
     }
 
     private <V> V getFieldValue(Object o, String fieldName) throws NoSuchFieldException, IllegalAccessException {
